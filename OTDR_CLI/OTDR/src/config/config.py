ca0a949e546e9d14fb1330f272bec2bff0549895
wadood <wadood2003@gmail.com>
2025-11-18 22:47:04 +0500

    added config file for easier access

---
+++ OTDR_CLI/OTDR/src/config/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+WANDB_API_KEY = os.getenv("WANDB_API_KEY")
+if OPENAI_API_KEY is None:
+    raise ValueError("OPENAI_API_KEY environment variable is not set. Please set it in your .env file.")
+if PINECONE_API_KEY is None:
+    raise ValueError("PINECONE_API_KEY environment variable is not set. Please set it in your .env file.")
+if WANDB_API_KEY is None:
+    raise ValueError("WANDB_API_KEY environment variable is not set. Please set it in your .env file.")
